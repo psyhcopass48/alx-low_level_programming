ed5efa9ccd8ee33198d71aac1377dcb58cd9f29a
psyhcopass48 <rhoul1946@gmail.com>
2023-03-16 19:00:25 -0700

    quote

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
- * * main - Entry point
- * *
- * * Return: Always 0 (Success)
- * */
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
 int main(void)
 {
 		printf("Size of a char: %d byte(s)\n", sizeof(char));
