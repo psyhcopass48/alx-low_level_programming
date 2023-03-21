518a33faf278e476debd237e0b504794231005c6
psyhcopass48 <rhoul1946@gmail.com>
2023-03-21 12:05:46 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,9 @@
-#include "main.h" 
-
+#include "main.h"
 /**
  * print_sign - Prints the sign of a number
  * @n: The number to be checked
  * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
-
 int print_sign(int n)
 
 {
@@ -20,7 +18,7 @@ int print_sign(int n)
 	_putchar(45);
 	return (-1);
 	}
-        else
+	else
 	{
 	_putchar(48);
 	return (0);
