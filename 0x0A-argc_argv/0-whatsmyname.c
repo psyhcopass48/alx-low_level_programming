dc753c3e6928a4f3d549f99d5d32d78bef852a23
psyhcopass48 <rhoul1946@gmail.com>
2023-04-11 14:36:03 -0700

    whatismyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,12 @@
 
 /**
  * main - prints the name of the program
- * @argc : number of arguments
- * @argv : array of arguments
- * Return : always 0 (success)
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0 (success)
  */
-int main(int argc __attribute__((unused)); char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n" , *argv);
+	printf("%s\n", *argv);
 	return (0);
-
+}
--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -3,9 +3,10 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c : character to print
- * return : 1 on success
- * on error , -1 is returned and errno is set appropriatly.
+ * @c: character to print
+ *
+ * return: On Success 1
+ * on error, -1 is returned and errno is set appropriatly.
  */
 int _putchar(char c)
 {
